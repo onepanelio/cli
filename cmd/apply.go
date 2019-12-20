bf00aa0291e704f0844458321d8464eed79c4a5e
Andrey Melnikov <vafilor@gmail.com>
2019-12-20 11:47:12 -0800

    Added apply.go

---
+++ cmd/apply.go
@@ -0,0 +1,171 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	opConfig "github.com/onepanelio/cli/config"
+	"github.com/onepanelio/cli/files"
+	"github.com/onepanelio/cli/template"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+// applyCmd represents the apply command
+var applyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "Applies a kubernetes yaml configuration file to your kubernetes cluster.",
+	Long: `Apples a kubernetes yaml configuration file given the 
+OpDef file and parameters file, 
+A sample usage is:
+
+op-cli apply config.yaml params.env
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("generate <path to config file>")
+			return
+		}
+
+		configFilePath := args[0]
+
+		config, err := opConfig.FromFile(configFilePath)
+		if err != nil {
+			fmt.Printf("Unable to read configuration file: %v", err.Error())
+			return
+		}
+
+		builder := template.NewBuilderFromConfig(*config)
+		if err := builder.Build(); err != nil {
+			log.Printf("Error generating config:  %v", err.Error())
+			return
+		}
+
+		kustomizeTemplate := builder.Template()
+
+		result, err := GenerateKustomizeResult(*config, kustomizeTemplate)
+		if err != nil {
+			log.Printf("Error generating result %v", err.Error())
+			return
+		}
+
+		finalKubernetesYamlFilePath := ".kubernetes.yaml"
+
+		exists, err := files.Exists(finalKubernetesYamlFilePath)
+		if err != nil {
+			log.Printf("Unable to check if file %v exists", finalKubernetesYamlFilePath)
+			return
+		}
+
+		var finalKubernetesFile *os.File = nil
+		if !exists {
+			finalKubernetesFile, err = os.Create(finalKubernetesYamlFilePath)
+			if err != nil {
+				log.Printf("Unable to create file: error %v", err.Error())
+				return
+			}
+		} else {
+			finalKubernetesFile, err = os.OpenFile(finalKubernetesYamlFilePath, os.O_RDWR|os.O_TRUNC, 0)
+			if err != nil {
+				log.Printf("Unable to open file: error %v", err.Error())
+				return
+			}
+		}
+
+		if _, err := finalKubernetesFile.WriteString(result); err != nil {
+			log.Printf("Error writing to temporary file: %v", err.Error())
+			return
+		}
+
+		fmt.Printf("Applying...")
+
+		res := ""
+		errRes := ""
+
+		for i := 0; i < 5; i++ {
+			res, errRes, err = applyKubernetesFile(finalKubernetesYamlFilePath)
+			if !strings.Contains(errRes, "no matches for kind") {
+				break
+			}
+
+			fmt.Printf(".")
+			fmt.Printf(".")
+
+			time.Sleep(time.Second * 3)
+		}
+
+		log.Printf("%v", res)
+
+		if err != nil {
+			fmt.Printf("\nFailed: %v", err.Error())
+		} else {
+			fmt.Printf("\nFinished applying\n")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(applyCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// applyCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func applyKubernetesFile(filePath string) (res string, errMessage string, err error) {
+	cmd := exec.Command("kubectl", "apply", "-f", filePath,  "--validate=false")
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", "", err
+	}
+
+	stdErr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", "", err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return "","", err
+	}
+
+	result, err := ioutil.ReadAll(stdOut)
+	if err != nil {
+		return "", "", err
+	}
+
+
+	errRes, err := ioutil.ReadAll(stdErr)
+	if err != nil {
+		return "", "", err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return string(result), string(errRes), err
+	}
+
+	return string(result), string(errRes), nil
+}
