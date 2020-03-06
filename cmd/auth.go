0422ab55ce03d42069ddd96ceeace4109174bdd1
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-03-06 15:53:04 -0800

    Properly implementing "auth token" as command sub-command

--- cmd/auth.go
+++ cmd/auth.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"github.com/onepanelio/cli/util"
 	"github.com/spf13/cobra"
-	"log"
 )
 
-var (
-	Token string
-)
 
 var authCmd = &cobra.Command{
 	Use:                        "auth",
-	Short:                      "Various provider auth commands.",
+	Short:                      "Get authentication information.",
 	Long:                       "Intented to be used to get authentication information.",
 	Example:                    "auth token",
 	Run: func(cmd *cobra.Command, args []string) {
-		if Token == "" {
-			log.Fatalln("Token cannot be empty.")
+	},
+}
+
+var tokenCmd = &cobra.Command{
+	Use: "token",
+	Short: "Get the token for a provider.",
+	Long: "Get a token for a given provider. Google Cloud Platform is different from minikube, for example.",
+	Example: "auth token --provider=gke",
+	Run: func(cmd *cobra.Command, args []string) {
+		if Provider == "" {
+			fmt.Println("Provider cannot be empty.")
+			return
 		}
+		if err := validateProvider(Provider); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
 		config := util.NewConfig()
 		token, err := util.GetBearerToken(config,"")
 		if err != nil {
@@ -28,9 +39,9 @@ var authCmd = &cobra.Command{
 		fmt.Println(token)
 	},
 }
-
 func init()  {
 	rootCmd.AddCommand(authCmd)
+	authCmd.AddCommand(tokenCmd)
 
-	authCmd.Flags().StringVarP(&Token,"token","","","Specify the provider to get auth token from. Valid values are the same as provider values.")
+	tokenCmd.Flags().StringVarP(&Provider,"provider","p","","Specify the provider to get auth token from. Valid values are the same as provider values.")
 }
