f18b6ae8e5d29a0684dca00f8124990d614bc75e
rushtehrani <r@onepanel.io>
2020-03-06 16:14:16 -0800

    remove --provider flag from auth

--- cmd/auth.go
+++ cmd/auth.go
@@ -2,46 +2,36 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/onepanelio/cli/util"
 	"github.com/spf13/cobra"
 )
 
-
 var authCmd = &cobra.Command{
-	Use:                        "auth",
-	Short:                      "Get authentication information.",
-	Long:                       "Intented to be used to get authentication information.",
-	Example:                    "auth token",
+	Use:     "auth",
+	Short:   "Get authentication information.",
+	Long:    "Intented to be used to get authentication information.",
+	Example: "auth token",
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
 
 var tokenCmd = &cobra.Command{
-	Use: "token",
-	Short: "Get the token for a provider.",
-	Long: "Get a token for a given provider. Google Cloud Platform is different from minikube, for example.",
+	Use:     "token",
+	Short:   "Get the token for a provider.",
+	Long:    "Get a token for a given provider. Google Cloud Platform is different from minikube, for example.",
 	Example: "auth token --provider=gke",
 	Run: func(cmd *cobra.Command, args []string) {
-		if Provider == "" {
-			fmt.Println("Provider cannot be empty.")
-			return
-		}
-		if err := validateProvider(Provider); err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
 		config := util.NewConfig()
-		token, err := util.GetBearerToken(config,"")
+		token, err := util.GetBearerToken(config, "")
 		if err != nil {
 			fmt.Println("Error encountered: ", err.Error())
 		}
 		fmt.Println(token)
 	},
 }
-func init()  {
+
+func init() {
 	rootCmd.AddCommand(authCmd)
 	authCmd.AddCommand(tokenCmd)
-
-	tokenCmd.Flags().StringVarP(&Provider,"provider","p","","Specify the provider to get auth token from. Valid values are the same as provider values.")
 }
