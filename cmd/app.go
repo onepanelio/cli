31ac85f3e0418d94240089f97b8aa45e962fc2c9
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-04-17 16:52:16 -0700

    Added ip sub-command.

--- cmd/app.go
+++ cmd/app.go
@@ -9,11 +9,20 @@ var appCmd = &cobra.Command{
 	Short:   "Various app functions.",
 	Long:    "Inspect and execute various app functions..",
 	Example: "app",
-	Run: func(cmd *cobra.Command, args []string) {
+	Run:     func(cmd *cobra.Command, args []string) {},
+}
 
+var ipCmd = &cobra.Command{
+	Use:     "ip",
+	Short:   "Get your cluster ip.",
+	Long:    "Will give instructions for setting up cluster ip and what it will be once set.",
+	Example: "opctl app ip",
+	Run: func(cmd *cobra.Command, args []string) {
+		print("opctl app ip")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(appCmd)
+	appCmd.AddCommand(ipCmd)
 }
