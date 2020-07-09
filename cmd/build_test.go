89904b45f2f74081dde07bc29b5fb1ad6d320a3a
rushtehrani <r@onepanel.io>
2020-07-09 11:19:46 -0700

    add test for generateApplicationNodePoolOptions

---
+++ cmd/build_test.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/onepanelio/cli/util"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	ParamsApplication = `application:
+  nodePool:
+    options:
+      - name: 'CPU: 2, RAM: 8GB'
+        value: Standard_D2s_v3
+      - name: 'CPU: 4, RAM: 16GB'
+        value: Standard_D4s_v3
+      - name: 'GPU: 1xK80, CPU: 6, RAM: 56GB'
+        value: Standard_NC6
+        resources:
+          limits:
+            nvidia.com/gpu: 1
+            cpu: 5000m
+            memory: 50000Mi`
+)
+
+func Test_generateApplicationNodePoolOptions(t *testing.T) {
+	// Have to account for extra spaces yaml.v3 seems to add
+	nodePoolOptionsExpected := `|
+    - name: 'CPU: 2, RAM: 8GB'
+      value: Standard_D2s_v3
+    - name: 'CPU: 4, RAM: 16GB'
+      value: Standard_D4s_v3
+    - name: 'GPU: 1xK80, CPU: 6, RAM: 56GB'
+      resources:
+          limits:
+              cpu: 5000m
+              memory: 50000Mi
+              nvidia.com/gpu: 1
+      value: Standard_NC6
+    
+`
+
+	data, err := util.LoadDynamicYamlFromString(ParamsApplication)
+	nodePoolData := data.GetValue("application.nodePool")
+	nodePoolOptionsActual := generateApplicationNodePoolOptions(nodePoolData)
+
+	assert.Nil(t, err)
+	assert.Equal(t, nodePoolOptionsExpected, nodePoolOptionsActual)
+}
