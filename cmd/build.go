bea78bcd4d1d33318e5603e4ffcae3f1170d5710
Aleksandr Melnikov <aleksandr@onepanel.io>
2020-07-10 17:20:25 -0700

    Changing the Secret key to match what we put into the config map.

--- cmd/build.go
+++ cmd/build.go
@@ -328,7 +328,7 @@ func GenerateKustomizeResult(config opConfig.Config, kustomizeTemplate template.
 			if val.Value == "" {
 				log.Fatal("artifactRepository.gcs.serviceAccountKey cannot be empty.")
 			}
-			artifactRepoS3Secret := "serviceAccountKeySecret: '" + val.Value + "'"
+			artifactRepoS3Secret := "serviceAccountKey: '" + val.Value + "'"
 			err = replacePlaceholderForSecretManiFile(localManifestsCopyPath, artifactRepoSecretPlaceholder, artifactRepoS3Secret)
 			if err != nil {
 				return "", err
